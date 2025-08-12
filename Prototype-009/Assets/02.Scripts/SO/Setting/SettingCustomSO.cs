6ceb438bf4098b4b44eb010509832d0d70f6f7d1
고한결 <gyeol0411@naver.com>
2025-08-12 18:55:39 +0900

    .update

--- Prototype-009/Assets/02.Scripts/SO/Setting/SettingCustomSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "BackgroundColorSO", menuName = "SO/Setting/BackgroundColorSO")]
-public class SettingCustomSO : ScriptableObject
-{
-    public Color BackgroundColor;
-}
---
+++ Prototype-009/Assets/02.Scripts/Utility/Unity/Common/Interpolater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Utility.Unity.Common
+{
+    public static class Interpolater
+    {
+        /// <summary>
+        /// float 을 lerp
+        /// </summary>
+        /// <param name="a">현재값</param>
+        /// <param name="b">목표값</param>
+        /// <param name="t">변화량</param>
+        /// <returns></returns>
+        public static float LerpFloat(float a, float b, float t)
+        {
+            return Mathf.Lerp(a, b, 1f - Mathf.Exp(-t * Time.deltaTime));
+        }
+    }
+}
